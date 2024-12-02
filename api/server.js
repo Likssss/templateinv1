06ab5f0ba5f6fe56df35a45bb74089c73817a5aa
Likssss <148707565+Likssss@users.noreply.github.com>
2024-12-02 12:18:17 +0700

    Add files via upload

--- api/server.js
+++ api/server.js
@@ -2,21 +2,23 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const fs = require('fs');
+const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 3001;  // Use the port provided by Vercel or default to 3001
+const port = process.env.PORT || 3001;
 app.use(cors());
 app.use(bodyParser.json());
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '..', 'build')));
 
 // Endpoint to save wishes
 app.post('/api/saveWish', (req, res) => {
     const { name, message } = req.body;
-    const filePath = './wishes.txt';
+    const filePath = path.join(__dirname, 'wishes.txt');
     const wishEntry = `Name: ${name}, Message: ${message}\n`;
 
-    // Check if the file exists
     if (fs.existsSync(filePath)) {
-        // Append to the existing file
         fs.appendFile(filePath, wishEntry, (err) => {
             if (err) {
                 console.error('Error appending to file:', err);
@@ -27,7 +29,6 @@ app.post('/api/saveWish', (req, res) => {
             res.send('Wishes appended successfully!');
         });
     } else {
-        // Create a new file and write data
         fs.writeFile(filePath, wishEntry, (err) => {
             if (err) {
                 console.error('Error writing to file:', err);
@@ -42,9 +43,7 @@ app.post('/api/saveWish', (req, res) => {
 
 // Endpoint to retrieve wishes
 app.get('/api/getWishes', (req, res) => {
-    const filePath = './wishes.txt';
-
-    // Check if the file exists
+    const filePath = path.join(__dirname, 'wishes.txt');
     if (fs.existsSync(filePath)) {
         fs.readFile(filePath, 'utf8', (err, data) => {
             if (err) {
@@ -58,6 +57,11 @@ app.get('/api/getWishes', (req, res) => {
     } else {
         res.send('No wishes found');
     }
+});
+
+// The "catchall" handler: for any request that doesn't match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
 });
 
 app.listen(port, () => {
